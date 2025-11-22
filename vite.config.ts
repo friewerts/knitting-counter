467533f2f8069b88c75af28b5de484b3df613c38
Frederik Riewerts <frederik.riewerts@gmail.com>
2025-11-22 17:07:42 +0100

    added pwa feature

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Sophie Scarf Counter',
+        short_name: 'Sophie',
+        description: 'Knitting counter for Sophie Scarf',
+        theme_color: '#fdfbf7',
+        background_color: '#fdfbf7',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   base: '/knitting-counter/',
 })
